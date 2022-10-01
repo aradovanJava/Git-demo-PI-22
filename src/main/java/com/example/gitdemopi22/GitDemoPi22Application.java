8a0c8d31d7e15d03c70d52b45ea62626f810cd9b
Aleksander <aleksande.radovan@gmail.com>
2022-10-01 11:34:24 +0200

    Initial commit

---
+++ src/main/java/com/example/gitdemopi22/GitDemoPi22Application.java
@@ -0,0 +1,13 @@
+package com.example.gitdemopi22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitDemoPi22Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitDemoPi22Application.class, args);
+    }
+
+}
