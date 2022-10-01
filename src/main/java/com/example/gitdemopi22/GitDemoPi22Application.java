50ae3f4ce1dc2ddcc881802cd2ca14275c08cb52
Aleksander <aleksande.radovan@gmail.com>
2022-10-01 11:39:44 +0200

    Testiranje prvog commita.

--- src/main/java/com/example/gitdemopi22/GitDemoPi22Application.java
+++ src/main/java/com/example/gitdemopi22/GitDemoPi22Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitDemoPi22Application {
 
     public static void main(String[] args) {
+        System.out.println("Evo aplikacije!");
         SpringApplication.run(GitDemoPi22Application.class, args);
     }
 
